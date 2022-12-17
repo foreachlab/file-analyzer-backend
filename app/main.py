8dd02cc08224f27a03b9d3c8e1562b33c6323c25
Yavuz <yavuzcekilis@gmail.com>
2022-12-17 11:14:05 +0300

    Add path and query parameters

--- app/main.py
+++ app/main.py
@@ -5,3 +5,12 @@ app = FastAPI()
 @app.get("/say_hi")
 async def root():
     return {"msg": "Hello World", "data": []}
+
+@app.get("/profile/{profile_id}")  
+async def get_profile_id(profile_id: int):
+    return { "msg": "" , "data": {"profile_id": profile_id}}
+
+@app.get("/component/")
+async def read_component(number: int, text: str):
+    number+= 1
+    return{"msg": "" , "data":{"number": number , "text" : text}}
